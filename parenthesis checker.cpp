a2b2f7aad86ba604c77b46a31d3f732cfec18818
vkvivek527 <32461817+vkvivek527@users.noreply.github.com>
2021-04-25 09:50:15 +0530

    Create parenthesis checker.cpp

---
+++ parenthesis checker.cpp
@@ -0,0 +1,78 @@
+https://practice.geeksforgeeks.org/problems/parenthesis-checker2744/1#
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+  public:
+    // Function to return if the paranthesis are balanced or not
+    bool ispar(string x)
+    {
+        stack<char> st;
+        char a;
+          
+          for(int i=0;i<x.length();i++){
+             if(x[i]=='(' || x[i]=='{' || x[i]=='['){
+                 st.push(x[i]);
+                 continue;
+             }
+             if(st.empty()){
+                 return 0;
+             }
+             
+             switch(x[i]){
+                 
+                 case ')':
+                a= st.top();
+                st.pop();
+                
+                if(a=='{' || a=='[')
+                return false;
+                 break;
+                 
+                 case '}':
+                  a= st.top();
+                st.pop();
+                
+                if(a=='(' || a=='[')
+                return false;
+                 break;
+                 
+                 case ']':
+                  a= st.top();
+                st.pop();
+                
+                if(a=='{' || a=='(')
+                return false;
+                 break;
+                 
+             }
+              
+          }
+          return st.empty();
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}  // } Driver Code Ends
