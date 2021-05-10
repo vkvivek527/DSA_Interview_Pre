2a84629c07c00ddf1d55d00677c6e20124e5e357
vkvivek527 <32461817+vkvivek527@users.noreply.github.com>
2021-05-11 01:14:29 +0530

    Create Isomorphic string.cpp

---
+++ Isomorphic string.cpp
@@ -0,0 +1,55 @@
+https://practice.geeksforgeeks.org/problems/isomorphic-strings-1587115620/1#
+
+
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+#define MAX_CHARS 256
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are isomorphic.
+    bool areIsomorphic(string str1, string str2)
+    {
+        int n=str1.length();
+        int m=str2.length();
+        if(n!=m){
+            return false;
+        }
+         int m1[256]={0};
+      int m2[256]={0};
+       
+        for(int i=0;i<n;i++){
+            if(!m1[str1[i]] and !m2[str2[i]]){
+                m1[str1[i]]=str2[i];
+                m2[str2[i]]=str1[i];
+            }else if(m2[str2[i]]!=str1[i]) {
+                return false;
+            }
+            
+        }
+     return true;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t;
+    cin>>t;
+    string s1,s2;
+    while (t--) {
+        cin>>s1;
+        cin>>s2;
+        Solution obj;
+        cout<<obj.areIsomorphic(s1,s2)<<endl;
+    }
+    
+    return 0;
+}  // } Driver Code Ends
